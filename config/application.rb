1d4d2427f8d7abf9a9c13c06c170649b5fb033d2
TakuyaHarayama <harayama.develop@gmail.com>
2019-12-14 21:11:57 +0900

    Configured config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,29 @@ module App
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # Generator settings
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.jbuilder false
+      g.test_framework :rspec,
+                       controller_specs: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false
+    end
+
+    # Default locale
+    config.i18n.default_locale = :ja
+
+    # Load locale files
+    config.i18n.load_path += Dir[
+      Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s
+    ]
+
+    # Timezone
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
