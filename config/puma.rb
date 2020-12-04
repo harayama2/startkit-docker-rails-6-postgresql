c197f98a9dd48b2883930a94022c81bbbb1db131
TakuyaHarayama <harayama.develop@gmail.com>
2020-12-04 12:22:50 +0900

    rubocop correct

--- config/puma.rb
+++ config/puma.rb
@@ -1,11 +1,11 @@
-workers ENV.fetch('WEB_CONCURRENCY') { 2 }
-threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
+workers ENV.fetch('WEB_CONCURRENCY', 2)
+threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
 threads threads_count, threads_count
 
 preload_app!
 
 rackup      DefaultRackup
-port        ENV.fetch('PORT') { 3000 }
+port        ENV.fetch('PORT', 3000)
 environment ENV.fetch('RAILS_ENV') { 'development' }
 
 on_worker_boot do
